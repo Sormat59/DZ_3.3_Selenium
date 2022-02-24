c294526f47ccfa65d62991c448178c0ec815125c
Sarmat <def2413@bk.ru>
2022-02-24 23:03:43 +0500

    Added test SendForm

--- src/test/java/ApplicationForm.java
+++ src/test/java/ApplicationForm.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationForm {
 
@@ -34,8 +37,18 @@ public class ApplicationForm {
     }
 
     @Test
-    public void shouldSendForm(){
+    public void shouldSendForm() {
         driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Салтыков-Щедрин Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79127850000");
+        driver.findElement(By.cssSelector(".checkbox__control")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success'] p" )).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        assertEquals (expected, actual);
+
+
 
     }
 
